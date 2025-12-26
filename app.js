7b5ea12f133f02e1d267a2de27df305e020fd879
min9v6nim <min9v6nim@gmail.com>
2025-12-26 15:08:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -162,3 +162,24 @@ document.getElementById("todayBtn").onclick=async ()=>{
   await fetchNotionEvents();
   render();
 })();
+
+async function fetchNotionEvents() {
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${NOTION_TOKEN}`,
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+  const data = await res.json();
+  console.log("노션 응답:", data); // 👈 이 줄 추가
+
+  notionEventsByDate = {};
+  ...
+}
+
